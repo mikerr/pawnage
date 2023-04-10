e4a35a915e849fc14077f26c9d8cbb8e1dc85758
Mike Redrobe <mike@redrobe.net>
2023-04-10 13:26:27 +0100

    Update pawnage.cpp

--- pawnage.cpp
+++ pawnage.cpp
@@ -1,30 +1,11 @@
 #include <string>
 #include <vector>
 
+#incldue "pawnage.h"
+
 #define BOARDSIZE 220
 #define WHITE 0
 #define BLACK 1
-
-
- struct Point {
-    int32_t x = 0, y = 0;
-
-    Point() = default;
-    constexpr Point(int32_t x, int32_t y) : x(x), y(y) {}
-
-    inline Point& operator-= (const Point &a) { x -= a.x; y -= a.y; return *this; }
-    inline Point& operator+= (const Point &a) { x += a.x; y += a.y; return *this; }
-    inline Point& operator*= (const float a) { x = static_cast<int32_t>(x * a); y = static_cast<int32_t>(y * a); return *this; }
-    inline Point& operator/= (const int32_t a) { x /= a;   y /= a;   return *this; }
-  };
-
-  inline bool operator== (const Point &lhs, const Point &rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }
-  inline bool operator!= (const Point &lhs, const Point &rhs) { return !(lhs == rhs); }
-  inline Point operator-  (Point lhs, const Point &rhs) { lhs -= rhs; return lhs; }
-  inline Point operator-  (const Point &rhs) { return Point(-rhs.x, -rhs.y); }
-  inline Point operator+  (Point lhs, const Point &rhs) { lhs += rhs; return lhs; }
-  inline Point operator*  (Point lhs, const float a) { lhs *= a; return lhs; }
-  inline Point operator/  (Point lhs, const int32_t a) { lhs /= a; return lhs; }
 
 std::string status;
 
